2f2fc0c21347e63ab77fdc9ab871a56c75318ef5
Rajesh <rajeshrathore05011998@gmail.com>
2021-06-10 17:30:16 +0530

    code clean

--- src/app/components/member-component/member/member.component.ts
+++ src/app/components/member-component/member/member.component.ts
@@ -18,19 +18,6 @@ export class MemberComponent implements OnInit {
   searchBar = true;
   membershipData :any = {};
   
-  allMembershipData = {
-    pesonalDetailOfMember:{
-      name:'Daya Ram Rajpoot',address:'vill & post Bhadsan, ajeetmal, auraiya, UP 206120',number:'9856452598',
-      email:'drraj112@gmail.com'
-    },
-    vehicleDetail:{
-      modelName: 'rajdoot 275', rcNumber: 'lyiktyuregtre', chechisNumber: 'XR10234STY324POI6', vehicleNumber:'UP 78 D 0343'
-    },
-    membershipDetail:{
-      membershipId:'uiyjtgfd8956',membershipStartDate:'22/05/2019',membershipEndDate:'22/05/2023'
-    }
-
-  }
   searchMember(event:any){
    
      this.memberService.getMember(event.value).subscribe((result) =>{
